ff50415c8892a1d161b4d32d0e9f74b049c3882b
Tony Lindgren <tony@atomide.com>
2020-08-03 10:44:47 +0300

    Update servers to point to 7 day xtra2.bin data
    Looks like the other servers only provide data for 4 hours:
    
    https://developer.qualcomm.com/comment/17055
    
    And the xtra2.bin file size is smaller when downloaded from
    xtrapath2.izatcloud.net compared to xtrapath6.izatcloud.net.

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 tmp_file=$(mktemp)
 
 echo "Downloading xtra2.bin to ${tmp_file}.."
-wget -O ${tmp_file} http://xtrapath2.izatcloud.net/xtra2.bin
+wget -O ${tmp_file} http://xtrapath6.izatcloud.net/xtra2.bin
 
 echo "Uplading xtra2.bin ${tmp_file} to mdm66oo.."
 ./droid4-agps --upload-only=${tmp_file}
